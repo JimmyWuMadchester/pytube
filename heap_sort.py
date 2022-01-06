c02aa39d25b8610b41faa86d1468e304441af9f6
Yulin Wu <darkato@icloud.com>
2022-01-06 20:11:35 +0000

    completed heap_sort

--- heap_sort.py
+++ heap_sort.py
@@ -1,10 +1,13 @@
 import pytest
 
 
-def max_heapify(arr: list, i: int):
+def max_heapify(arr: list, heap_size: int, i: int):
+    """
+    Maintain max-heap property at node i.
+    i.e. makes arr[i] "float down"
+    """
     l = (i << 1) + 1
     r = (i << 1) + 2
-    heap_size = len(arr)
     largest = i
 
     if l < heap_size and arr[l] > arr[i]:
@@ -15,13 +18,20 @@ def max_heapify(arr: list, i: int):
 
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
-        max_heapify(arr, largest)
+        max_heapify(arr, heap_size, largest)
 
 
 def build_max_heap(arr: list):
     heap_size = len(arr)
     for i in range(heap_size // 2 - 1, -1, -1):
-        max_heapify(arr, i)
+        max_heapify(arr, heap_size, i)
+
+
+def heap_sort(arr: list):
+    build_max_heap(arr)
+    for i in range(len(arr) - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        max_heapify(arr, i, 0)
 
 
 @pytest.mark.parametrize("arr, expected",
@@ -61,4 +71,27 @@ def test_max_heapify(arr: list, expected: list):
                          ])
 def test_build_max_heap(arr: list, expected: list):
     build_max_heap(arr)
+    assert arr == expected
+
+
+@pytest.mark.parametrize("arr, expected",
+                         [
+                             pytest.param(
+                                 [4, 16, 10], [4, 10, 16]
+                             ),
+                             pytest.param(
+                                 [16, 4, 10, 14, 7, 9, 3, 2, 8, 1], [1, 2, 3, 4, 7, 8, 9, 10, 14, 16]
+                             ),
+                             pytest.param(
+                                 [], []
+                             ),
+                             pytest.param(
+                                 [5], [5]
+                             ),
+                             pytest.param(
+                                 [1, 1, 1, 1], [1, 1, 1, 1]
+                             ),
+                         ])
+def test_heap_sort(arr: list, expected: list):
+    heap_sort(arr)
     assert arr == expected
